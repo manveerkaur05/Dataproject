9218b3e3ac912184b8481f9e8e66862900dab10a
manveerkaur05 <140010860+manveerkaur05@users.noreply.github.com>
2024-03-19 13:41:30 -0500

    user select their favorite book

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -5,4 +5,5 @@ class Favorite < ApplicationRecord
   validates :user_id, presence: true
   validates :book_id, presence: true
   validates :user_id, uniqueness: { scope: :book_id, message: "already favorited this book" }
+  
 end
